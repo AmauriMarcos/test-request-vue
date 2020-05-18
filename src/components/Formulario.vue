8fd812f17e1e07191ed5ae33dfe3ff4c86b37236
AmauriMarcos <amaurisantos.m@gmail.com>
2020-05-18 18:47:57 +0200

    setup get and post data

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -18,7 +18,23 @@
          </v-text-field>
          <v-btn small color="deep-purple accent-4" class='myButton' @click='submit()'>Submit</v-btn>
       </v-form>
-      <p></p>
+     
+      <div class="box-info">
+         <v-btn small color="success" class='myButton' @click='getData()'>Get data</v-btn>
+        <v-card
+          class="mx-auto"
+          tile
+          v-for='dados in info' :key='dados'
+        >
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>{{dados.name}}</v-list-item-title>
+              <v-list-item-subtitle>{{dados.email}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+ 
+      </v-card>
+      </div>
     </v-app>
   
   </div>
@@ -38,13 +54,19 @@ export default {
     }
   },
   methods:{
-      submit(){
-        axios.post("https://vueserver-80315.firebaseio.com/data.json", this.user)
-        .then(response => {
-           console.log(response);
-        }).catch(error =>{
-           console.log(error);
-        })
+    submit(){
+      axios.post("https://vueserver-80315.firebaseio.com/data.json", this.user)
+      .then(response => {
+          console.log(response);
+      }).catch(error =>{
+          console.log(error);
+      })
+    },
+     async getData(){
+      let res = await  axios.get("https://vueserver-80315.firebaseio.com/data.json")
+       for(let key in res.data){
+         this.info.push(res.data[key]);
+       }
     }
   }
 };
@@ -59,11 +81,17 @@ export default {
     padding: 1.5rem;
    
   }
+  .box-info{
+    margin-top: 2rem;
+    width: 100%;
+  }
   .v-application--wrap{
     min-height: 0 !important;
   }
 
   .myButton{
     color: white !important;
+    margin-bottom: 1.5rem;
   }
+  
 </style>
